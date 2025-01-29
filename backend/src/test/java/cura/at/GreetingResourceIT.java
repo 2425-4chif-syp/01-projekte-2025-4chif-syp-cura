2708b09391fa94ad0fa574572ba7b45394eecce6
MistelbergerLeopold <l.mistelberger@students.htl-leonding.ac.at>
2025-01-29 11:57:53 +0100

    added simple quarkus application

---
+++ backend/src/test/java/cura/at/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package cura.at;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
