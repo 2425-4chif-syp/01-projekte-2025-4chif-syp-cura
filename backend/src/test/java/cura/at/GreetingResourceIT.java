690465661947705ef1a3d35d4699ed5edea110e4
MistelbergerLeopold <l.mistelberger@students.htl-leonding.ac.at>
2025-02-04 13:57:04 +0100

    deleted quarkus backend becuase it is not necesery

--- backend/src/test/java/cura/at/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package cura.at;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
