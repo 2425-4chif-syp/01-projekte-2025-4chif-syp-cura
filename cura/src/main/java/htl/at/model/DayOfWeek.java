f3292199c2692b4835372f7e0cf5edaf341ad109
MistelbergerLeopold <l.mistelberger@students.htl-leonding.ac.at>
2025-04-10 12:43:31 +0200

    feat: add DayOfWeek Enum

---
+++ cura/src/main/java/htl/at/model/DayOfWeek.java
@@ -0,0 +1,14 @@
+package htl.at.model;
+
+/**
+ * Enumeration representing days of the week
+ */
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
