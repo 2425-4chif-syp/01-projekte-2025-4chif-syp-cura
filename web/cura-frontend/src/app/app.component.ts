6ef658f2df75837e2850bd72c8fd6f7ba0288ca1
EliasMahr07 <114005710+EliasMahr07@users.noreply.github.com>
2025-10-22 08:33:50 +0200

    feat: webside header

--- web/cura-frontend/src/app/app.component.ts
+++ web/cura-frontend/src/app/app.component.ts
@@ -9,4 +9,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cura-frontend';
+  currentDate = new Date().toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: '2-digit' });
 }
