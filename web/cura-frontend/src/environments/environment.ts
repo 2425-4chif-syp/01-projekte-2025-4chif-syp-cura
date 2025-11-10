24abc8a7c5eb9b5daa9ecdab948dc87441e62c83
EliasMahr07 <114005710+EliasMahr07@users.noreply.github.com>
2025-11-10 21:50:07 +0100

    feat: keycloak

--- web/cura-frontend/src/environments/environment.ts
+++ web/cura-frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://vm12.htl-leonding.ac.at/api',     // ✅ Backend auf Server
+  apiUrl: 'https://vm12.htl-leonding.ac.at/api',     // ✅ Backend auf Server
   keycloak: {
-    url: 'http://vm12.htl-leonding.ac.at/auth',    // ✅ Keycloak auf Server
+    url: 'https://vm12.htl-leonding.ac.at/auth',    // ✅ Keycloak auf Server
     realm: 'cura',
     clientId: 'cura-frontend'
   }
