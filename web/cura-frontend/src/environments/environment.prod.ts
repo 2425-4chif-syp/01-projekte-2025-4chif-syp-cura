63edc07edcdbe5f4becb245b756b417abb3097f4
EliasMahr07 <114005710+EliasMahr07@users.noreply.github.com>
2025-11-13 11:28:48 +0100

    test: environment

--- web/cura-frontend/src/environments/environment.prod.ts
+++ web/cura-frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://vm12.htl-leonding.ac.at/api',
   keycloak: {
-    url: 'https://vm12.htl-leonding.ac.at/auth',
+    url: 'https://vm12.htl-leonding.ac.at/auth/',
     realm: 'cura',
     clientId: 'cura-frontend'
   }
