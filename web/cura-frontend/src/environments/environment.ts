08af80f01ab6220adcbf721980fa82f31c439ccc
EliasMahr07 <114005710+EliasMahr07@users.noreply.github.com>
2025-11-13 12:19:06 +0100

    fix: environment

--- web/cura-frontend/src/environments/environment.ts
+++ web/cura-frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://vm12.htl-leonding.ac.at/api',     // ✅ Backend auf Server
   keycloak: {
-    url: 'https://vm12.htl-leonding.ac.at/auth/',    // ✅ Keycloak auf Server
+    url: 'https://vm12.htl-leonding.ac.at/auth',    // ✅ Keycloak auf Server
     realm: 'cura',
     clientId: 'cura-frontend'
   }
